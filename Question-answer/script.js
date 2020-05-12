798f58a7fd7b26dda8e3a00003f3422e7830507b
Aliyaman <ali4yaman@gmail.com>
2020-05-12 20:44:15 +0300

    commit

---
+++ Question-answer/script.js
@@ -0,0 +1,40 @@
+var soru = prompt("Komutlar:kimsin,blog,site,github,geliştiricin,ali");
+if (soru == "kimsin") {
+    alert("ben yapay zeka olmayan yapay zekayım. :)");
+}
+if (soru == "blog") {
+    alert("https://aliyaman.github.io/mukemmel-blog(Bakarsan Sevinirim)");
+}
+if (soru == "site") {
+    alert("http://aliyaman.net");
+}
+if (soru == "github") {
+    alert("https://github.com/Aliyaman");
+}
+if (soru == "geliştiricin") {
+    alert("Ali Yaman");
+}
+if (soru == "ali") {
+    alert("O zekidir,Yazılım Yapmayı Sever(Yakında Biyografi Geliyor)")
+}
+function newQuestion() {
+    var soru = prompt("Komutlar:kimsin,blog,site,github,geliştiricin")
+    if (soru == "kimsin") {
+        alert("ben yapay zeka olmayan yapay zekayım. :)");
+    }
+    if (soru == "blog") {
+        alert("https://aliyaman.github.io/mukemmel-blog(Bakarsan Sevinirim)");
+    }
+    if (soru == "site") {
+        alert("http://aliyaman.net");
+    }
+    if (soru == "github") {
+        alert("https://github.com/Aliyaman");
+    }
+    if (soru == "geliştiricin") {
+        alert("Ali Yaman");
+    }
+    if (soru == "ali") {
+        alert("O zekidir,Yazılım Yapmayı Sever(Yakında Biyografi Geliyor)")
+    }
+}
